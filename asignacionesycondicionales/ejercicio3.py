7a63d21209fd66944856cfd930fc74c86b50080e
torjaquintero <torjaquintero@gmail.com>
2024-01-11 10:29:06 -0500

    Create ejercicio3.py

---
+++ asignacionesycondicionales/ejercicio3.py
@@ -0,0 +1,16 @@
+# Bienvenidos a Sys On Chip
+# ==============================
+# Ejercicio No.3
+# Programa que captura dos datos numéricos por el teclado y luego los suma.
+# El resultado de la operación lo muestra en pantalla
+# ==============================================================================================
+
+#guardamos los numeros en dos variables numero1 y numero2
+#la instruccion int() asegura que el usuario ingrese un numero entero
+numero1 = int(input("Digite primer numero a sumar: ")
+numero2 = int(input("Digite primer numero a sumar: ")
+
+#el resultado de sumar los numeros se guarda en la variable suma 
+suma = numero1 + numero2
+
+print('El resultado de la suma:', numero1, '+', numero2, 'es: ', suma)
