08d110466eecb0714469606abd5039d78fea722e
torjaquintero <torjaquintero@gmail.com>
2024-01-11 18:48:20 -0500

    Create ejercicio10.py

---
+++ asignacionesycondicionales/ejercicio10.py
@@ -0,0 +1,12 @@
+# Bienvenidos a Sys On Chip
+# ==============================
+# Ejercicio No.10
+# Programa que imprime un menú y pide que se elija una opción
+# ==============================================================================================
+
+print('1. Ingrese nombre de cliente: ')
+print('2. Ingrese numero de cedula: ')
+print('3. Ingresa numero de telefono: ')
+
+opcion = int(input('Elija una opcion: '))
+print('Usted ha elegido la opción', opcion)
