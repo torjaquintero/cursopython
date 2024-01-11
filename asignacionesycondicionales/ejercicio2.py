8c9f0ca3d2fbce1946b705bd207038334a2df289
torjaquintero <torjaquintero@gmail.com>
2024-01-11 10:25:21 -0500

    Create ejercicio2.py

---
+++ asignacionesycondicionales/ejercicio2.py
@@ -0,0 +1,14 @@
+# Bienvenidos a Sys On Chip
+# ==============================
+# Ejercicio No.2
+# Este programa captura datos ingresados a través del teclado y
+# los guarda en las variables nombre y numeroid para luego imprimirlos en pantalla
+# ==============================================================================================
+
+#ingresamos por teclado el nombre y la identificacion del usuario
+nombre = input('Digite su nombre: ')
+numeroid = input('Digite su numero de identificación: ')
+
+#muestra en pantalla el nombre y la identificacion del usuario
+print('\nEl nombre del usuario es: ', nombre)
+print('El número de identificacion del usuario es : ', numeroid)
