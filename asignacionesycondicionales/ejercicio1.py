b7f749b34694584fd491a551b54856e17f9cf49f
torjaquintero <torjaquintero@gmail.com>
2024-01-11 10:22:50 -0500

    Create ejercicio1.py

---
+++ asignacionesycondicionales/ejercicio1.py
@@ -0,0 +1,8 @@
+# Bienvenidos a Sys On Chip
+# ==============================
+# Ejercicio No.1
+# Este programa imprime en pantalla un mensaje de texto
+# ==============================================================================================
+
+# Imprime un mensaje en pantalla
+print('Bienvenidos al curso de Python desde cero')
